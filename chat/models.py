fa15bec778d8ae3db5417674b124337a27eda47e
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-01-27 22:17:13 +0300

    create model (Message) for chat

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import Record, User
 
-# Create your models here.
+class Message(models.Model):
+    def __init__(self, **kwargs):
+        obj = super(Message, self).__init__(**kwargs)
+        Record.objects.create(**kwargs)
+        return obj
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
