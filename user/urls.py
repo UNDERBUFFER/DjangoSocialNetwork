a3df307538c42d821013e9b208e7ae9c773390ca
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-01-27 14:33:11 +0300

    minimal views preparing

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-from .views import all_walls, wall
+from .views import all_walls, photos, records, wall
 from admission.views import not_found
 from django.contrib import admin
 from django.urls import include, path, re_path
@@ -6,5 +6,7 @@ from django.urls import include, path, re_path
 urlpatterns = [
     path('/all_walls', all_walls, name='all_walls'),    
     path('/wall/<int:iden>', wall, name='wall'),
+    path('/wall/<int:iden>/photos', photos, name='photos'),
+    path('/wall/<int:iden>/records', records, name='records'),
     re_path(r'^.*$', not_found),
 ]
--- user/views.py
+++ user/views.py
@@ -7,12 +7,12 @@ def all_walls(request):
 	objects = [(i.id, i.username) for i in objects]
 	return render(request, 'user/all_walls.html', context={'objects': objects})
 
-def create_response(request, temp, iden):
-	context = {}
-	context['username'] = User.objects.get(id=iden).username
-	context['records'] = [i.text for i in Record.objects.filter(author=iden)]
-	context['photos'] = [i.photo.name for i in Photo.objects.filter(author=iden)]
-	return render(request, temp, context=context)
+#def create_response(request, temp, iden):
+#	context = {}
+#	context['username'] = User.objects.get(id=iden).username
+#	context['records'] = [i.text for i in Record.objects.filter(author=iden)]
+#	context['photos'] = [i.photo.name for i in Photo.objects.filter(author=iden)]
+#	return render(request, temp, context=context)
 
 def wall(request, iden):
 	wall = 'other'
@@ -23,10 +23,18 @@ def wall(request, iden):
 	if obj is not None:
 		if iden == obj.id:
 			wall = 'your'
-			if request.POST.get('text', None) not in [None, '']:
-				Record.objects.create(author=obj, text=request.POST['text'])
-			if request.FILES.get('file', None) is not None:
-				Photo.objects.create(author=obj, photo=File(request.FILES['file']))
+	username = User.objects.get(id=iden).username
+	#		if request.POST.get('text', None) not in [None, '']:
+	#			Record.objects.create(author=obj, text=request.POST['text'])
+	#		if request.FILES.get('file', None) is not None:
+	#			Photo.objects.create(author=obj, photo=File(request.FILES['file']))
 	if wall == 'your':
-		return create_response(request, 'user/your_wall.html', iden)
-	return create_response(request, 'user/other_wall.html', iden)
+		return render(request, 'user/your_wall.html', context={'username': username})
+	#	return create_response(request, 'user/your_wall.html', iden)
+	return create_response(request, 'user/other_wall.html', context={'username': username})
+
+def photos(request):
+	pass
+
+def records(request):
+	pass
