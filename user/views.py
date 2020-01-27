0bdb8aaa1a5d5d37c1d0caafed41fcc52f7d72bd
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-01-27 16:16:47 +0300

    the last virsion at the moment with all fixs

--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from .models import User, Record, Photo
 from django.core.files import File
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
@@ -8,13 +7,6 @@ def all_walls(request):
 	objects = User.objects.all()
 	objects = [(i.id, i.username) for i in objects]
 	return render(request, 'user/all_walls.html', context={'objects': objects})
-
-#def create_response(request, temp, iden):
-#	context = {}
-#	context['username'] = User.objects.get(id=iden).username
-#	context['records'] = [i.text for i in Record.objects.filter(author=iden)]
-#	context['photos'] = [i.photo.name for i in Photo.objects.filter(author=iden)]
-#	return render(request, temp, context=context)
 
 def whose(request, iden):
 	this = 'other'
@@ -34,14 +26,9 @@ def wall(request, iden):
 		return redirect(reverse('records', args=[iden]))
 	obj = User.objects.get(id=iden)
 	username = obj.username
-	#		if request.POST.get('text', None) not in [None, '']:
-	#			Record.objects.create(author=obj, text=request.POST['text'])
-	#		if request.FILES.get('file', None) is not None:
-	#			Photo.objects.create(author=obj, photo=File(request.FILES['file']))
 	this = whose(request, iden)
 	if this == 'your':
 		return render(request, 'user/your_wall.html', context={'username': username})
-	#	return create_response(request, 'user/your_wall.html', iden)
 	return render(request, 'user/other_wall.html', context={'username': username})
 
 def photos(request, iden):
@@ -54,7 +41,6 @@ def photos(request, iden):
 			Photo.objects.create(author=obj, photo=File(request.FILES['file']))
 			photos = [i.photo.name for i in Photo.objects.filter(author=iden)]
 		return render(request, 'user/your_photos.html', context={'username': username, 'photos': photos})
-	#	return create_response(request, 'user/your_wall.html', iden)
 	return render(request, 'user/other_photos.html', context={'username': username, 'photos': photos})
 
 def records(request, iden):
@@ -67,5 +53,4 @@ def records(request, iden):
 			Record.objects.create(author=obj, text=request.POST['text'])
 			records = [i.text for i in Record.objects.filter(author=iden)]
 		return render(request, 'user/your_records.html', context={'username': username, 'records': records})
-	#	return create_response(request, 'user/your_wall.html', iden)
-	return render(request, 'user/other_records.html', context={'username': username, 'recordss': records})
+	return render(request, 'user/other_records.html', context={'username': username, 'records': records})
