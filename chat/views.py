f223de1e6116d7dfa3baa1fde2107a72b0887d28
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-01-29 13:58:52 +0300

    removing unnecessary code

--- chat/views.py
+++ chat/views.py
@@ -8,30 +8,19 @@ def chat(request):
     if (res := access(request)) is not True:
         return res
     who = request.user
-    username = who.username
     messages = Message.objects.all()
     if request.POST.get('text', None) not in [None, '']:
         Message.objects.create(author=request.user, text=request.POST['text'])
         messages = Message.objects.all()
     ignores = Ignore.objects.filter(who=who)
-    #mess = []
-    #for message in messages:
-    #    bool = True
-    #    for ignore in ignores:
-    #        if ignore.whom == message.author:
-    #            bool = False
-    #    if bool is True:
-    #        mess.append(message)
     mess = correct_messages(ignores, messages)[-8:]
-    #mess = mess
-    return render(request, 'chat/chat.html', context={'username': username, 'messages': mess})
+    return render(request, 'chat/chat.html', context={'username': who.username, 'messages': mess})
 
 def settings(request):
     if (res := access(request)) is not True:
-        return res
-        
+        return res 
+    who = request.user
     if request.POST.get('whom', None) not in [None, '']:
-        who = request.user
         whom = User.objects.get(id=request.POST['whom'])
         if who.id == whom.id:
             pass
@@ -39,4 +28,5 @@ def settings(request):
             pass
         else:
             Ignore.objects.create(who=who, whom=whom)
-    return render(request, 'chat/settings.html')
+    ignores = Ignore.objects.filter(who=who)
+    return render(request, 'chat/settings.html', context={'username': who.username, 'ignores': ignores})
