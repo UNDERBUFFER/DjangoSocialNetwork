157cf38c3146b0f158c29d3d5f880540600a004b
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-01-29 11:40:24 +0300

    minimal functional

--- chat/functions.py
+++ chat/functions.py
@@ -4,3 +4,14 @@ def access(request):
 	if request.user.is_anonymous:
 		return redirect('/')
 	return True
+
+def correct_messages(ignores, messages):
+	mess = []
+	for message in messages:
+		bool = True
+		for ignore in ignores:
+			if ignore.whom == message.author:
+				bool = False
+		if bool is True:
+			mess.append(message)
+	return mess
--- chat/views.py
+++ chat/views.py
@@ -1,19 +1,42 @@
-from .functions import access
-from .models import Message
+from .functions import access, correct_messages
+from .models import Message, Ignore
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from user.models import User
 
 def chat(request):
     if (res := access(request)) is not True:
         return res
-    username = request.user.username
-    messages = Message.objects.all().order_by('-id')[:8]
+    who = request.user
+    username = who.username
+    messages = Message.objects.all()
     if request.POST.get('text', None) not in [None, '']:
         Message.objects.create(author=request.user, text=request.POST['text'])
-        messages = Message.objects.all().order_by('-id')[:8]
-    return render(request, 'chat/chat.html', context={'username': username, 'messages': messages})
+        messages = Message.objects.all()
+    ignores = Ignore.objects.filter(who=who)
+    #mess = []
+    #for message in messages:
+    #    bool = True
+    #    for ignore in ignores:
+    #        if ignore.whom == message.author:
+    #            bool = False
+    #    if bool is True:
+    #        mess.append(message)
+    mess = correct_messages(ignores, messages)[-8:]
+    #mess = mess
+    return render(request, 'chat/chat.html', context={'username': username, 'messages': mess})
 
 def settings(request):
     if (res := access(request)) is not True:
         return res
+        
+    if request.POST.get('whom', None) not in [None, '']:
+        who = request.user
+        whom = User.objects.get(id=request.POST['whom'])
+        if who.id == whom.id:
+            pass
+        elif len(Ignore.objects.filter(who=who, whom=whom)) != 0:
+            pass
+        else:
+            Ignore.objects.create(who=who, whom=whom)
     return render(request, 'chat/settings.html')
