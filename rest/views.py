3b1f1b3f8b8644fe24a5251be2bd585f3ec01ce3
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-02-14 21:46:17 +0300

    fucking recursive delete method

--- rest/views.py
+++ rest/views.py
@@ -4,18 +4,28 @@ from django.shortcuts import redirect, render
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 from user.models import User
 
-class ViewOneObject(APIView):
+class ViewOneObject(GenericAPIView):
     serializer_class = UserSerializer
     def get(self, request, iden):
         try:
             obj = UserSerializer(Backend().get_user(iden))
         except:
-            pass
+            return Response('')
         return Response(obj.data)
     def post(self, request, iden):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
+        return redirect('/rest/{}'.format(User.objects.last().id))
+    def put(self, request, iden):
+        serializer = UserSerializer(instance=Backend().get_user(iden), data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return redirect('/rest/{}'.format(iden))
+    def delete(self, request, iden):
+        Backend().get_user(iden).delete()
+        #return Response('')
         return redirect('/rest/{}'.format(User.objects.last().id))
