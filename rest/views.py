7f73ac5c642aef680139f6f09ae528199ee5e5a9
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-02-15 15:57:20 +0300

    first work version with other views

--- rest/views.py
+++ rest/views.py
@@ -1,5 +1,4 @@
 from .serializers import CreateSerializer, ViewSerializer
-from admission.backend import Backend
 from django.shortcuts import redirect, render
 from rest_framework.generics import GenericAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
@@ -8,6 +7,10 @@ from user.models import User
 
 class CreateOneObject(CreateAPIView):
     serializer_class = CreateSerializer
+    def post(self, request):
+        old = super().post(request)
+        data = {'detail': 'user is on rest/{}'.format(User.objects.get(email=old.data["email"]).id)}
+        return Response(data)
 
 class ViewOneObject(RetrieveUpdateDestroyAPIView):
     serializer_class = ViewSerializer
