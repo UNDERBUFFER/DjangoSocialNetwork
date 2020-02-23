846eee6268ed80493a44bc74191e5362c9e23c6d
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-02-23 14:02:15 +0300

    I must remember this

---
+++ user/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from user.models import *
+
+class GETUser(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+class GETPUTDELETEUser(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class GETPOSTRecord(serializers.ModelSerializer):
+    class Meta:
+        model = Record
+        fields = ['text']
+
+class GETPOSTPhoto(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = ['photo']
