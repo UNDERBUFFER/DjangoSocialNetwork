f201c4a7c111fbc5c16d10b41485f725b38232dc
DolphinHMPY <52374782+DolphinHMPY@users.noreply.github.com>
2020-02-26 22:52:21 +0300

    also utils.py

---
+++ web_app/chat/utils.py
@@ -0,0 +1,14 @@
+import json
+import requests
+from django.shortcuts import redirect, render
+from user.utils import AccessException
+
+def access_request(request, url):
+    session = requests.session()
+    token = request.COOKIES.get('Authorization', '')
+    rest = json.loads(session.get(url, headers={'Authorization': token}).text)
+    try:
+        if rest.get('detail') is not None:
+            return False
+    except:
+        return (True, rest)
